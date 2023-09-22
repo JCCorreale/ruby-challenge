2284a412005ede1348c6663cc9cdc434a8f75794
Jean Claude Correale <jeanclaudecorreale@gmail.com>
2023-09-22 12:54:26 +0200

    level 1 - write output and diff check

--- level1/main.rb
+++ level1/main.rb
@@ -1,10 +1,13 @@
 require "json"
 require "holidays"
+require "hashdiff"
 
 def isWeekend(date)
     return date.saturday? || date.sunday?
 end
 
+example_output_filename = "output.json"
+output_filename = "my_output.json"
 input_filename = 'data.json' 
 dateformat = "%Y-%m-%d"
 locale = :it
@@ -14,7 +17,7 @@ begin
 rescue Errno::ENOENT
     puts  + "#{input_filename} does not exist"
 rescue => e
-    puts "error accessing #{input_filename} : #{e.message}"
+    puts "error reading #{input_filename} : #{e.message}"
 end
 
 unless file 
@@ -24,36 +27,49 @@ end
 data_hash = JSON.parse file
 output_hash = {}
 
-data_hash["periods"].each do |period|
-    from = Date.strptime(period["since"], dateformat)
-    to = Date.strptime(period["until"], dateformat)
+availabilities_hash = { "availabilities" => data_hash["periods"].map do |period|
+        from = Date.strptime(period["since"], dateformat)
+        to = Date.strptime(period["until"], dateformat)
 
-    holidays = Holidays.between(from, to, locale).map { |h| h[:date] }
+        holidays = Holidays.between(from, to, locale).map { |h| h[:date] }
 
-    days = (from..to)
+        days_count = (from..to).reduce({ "weekend" => 0, "work" => 0, "holidays" => 0 }) do |obj, day|
 
-    # check different object notation
-    days_count = days.reduce({ "weekend" => 0, "work" => 0, "holidays" => 0 }) do |obj, day|
-
-        is_weekend = isWeekend(day)
-        
-        if (holidays.include? day) && !is_weekend
-            obj["holidays"] += 1
-        elsif is_weekend
-            obj["weekend"] += 1
-        else 
-            obj["work"] += 1
+            is_weekend = isWeekend(day)
+            
+            if (holidays.include? day) && !is_weekend
+                obj["holidays"] += 1
+            elsif is_weekend
+                obj["weekend"] += 1
+            else 
+                obj["work"] += 1
+            end
+            obj
         end
-        obj # no "return"!
-    end
 
-    availability = {
-        period_id: period["id"],
-        total_days: days.size(),
-        holidays: days_count["holidays"],
-        weekend_days: days_count["weekend"],
-        workdays: days_count["work"]
+        {
+            "period_id" => period["id"],
+            "total_days" => (to - from).to_i + 1,
+            "holidays" => days_count["holidays"],
+            "weekend_days" => days_count["weekend"],
+            "workdays" => days_count["work"]
+        }
+
+        end
     }
 
-    puts availability
+
+begin
+    File.write(output_filename, JSON.pretty_generate(availabilities_hash))
+rescue => e
+    puts "error writing #{output_filename} : #{e.message}"
+end
+
+# double check
+example_output = JSON.parse File.read example_output_filename
+diff = Hashdiff.diff(availabilities_hash, example_output)
+if diff
+    puts "OK"
+else
+    puts "KO"
 end
